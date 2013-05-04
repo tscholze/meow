957920d4a7fb7d76288c69909080d9e4b9693f05
Klaus Meyer <spam@klaus-meyer.net>
2013-05-04 15:36:42 +0200

    linking to rss feed in html-header and site-footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Cats::Application.routes.draw do
   root :to => 'cats#index'
   get '/cats/random' => 'cats#random', :as => :random_cat
   get '/cats/:id' => 'cats#show', :as => :cat
-  get '/feed' => 'cats#feed'
+  get '/feed' => 'cats#feed', :as => :feed
   delete '/cats/:id' => 'cats#destroy'
   match '/users/login', :as => :user_login
   match '/users/logout', :as => :user_logout
